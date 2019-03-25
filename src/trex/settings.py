69ac7e632dc7b6fef790d25cf97df1291f26cd3f
Holden Maudlin <holdenmaudlin@Holdens-MacBook-Pro.local>
2019-03-25 13:13:25 -0700

    static

--- src/trex/settings.py
+++ src/trex/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!5e#(k420ll^c)4)788p3-rv29$qs-c#5iqn&je0*1!#jfp5s&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['trex-dev.us-west-1.elasticbeanstalk.com']
 
 
 # Application definition
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
